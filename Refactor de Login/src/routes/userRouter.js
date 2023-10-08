0c86b1d7de7fd7f507834b733655e2651aeaf607
David Ferere <davidferere@gmail.com>
2023-10-08 13:12:41 -0400

    Update userRouter.js

--- Refactor de Login/src/routes/userRouter.js
+++ Refactor de Login/src/routes/userRouter.js
@@ -74,19 +74,12 @@ router.post('/login', async (req, res) => {
         return res.send("Credenciales INVALIDAS");
     }
 
-    //const user = await usersModel.create({ username, email, password });
-    //console.log(username); 
     //guardamos info del usuario en session
     req.session.username = username;
     req.session.email = user.email;
     req.session.isLogged = true;
 
-    //res.send("Bienvenido");
-
     res.redirect('/mongo/products/catalog');
-
-    //res.redirect('/ecommerce/home/profile');
-
 
 
 });
@@ -107,35 +100,8 @@ router.get(
         req.session.email = req.user.email;
         req.session.age = req.user.age;
         req.session.isLogged = true;
-        //res.redirect('/ecommerce/home/profile');
         res.redirect('/mongo/products/catalog'); 
     }); 
 
-
-/*router.get('/profile', async (req, res) => {
-
-    const { username, email} = req.session;
-    console.log(username);
-
-    //const userExists = await usersModel.findOne({ email });
-
-    /*if (userExists) { //validamos si usuario ya existe en la BD
-        return res.send("El usuario ya existe");
-    }*/
-
-//const user = await usersModel.create({ username, email, password });
-//console.log(username); 
-//guardamos info del usuario en session
-/*req.session.username = username;
-req.session.email = email;
-req.session.isLogged = true;*/
-
-//res.send("Bienvenido");
-
-/*res.render('/profile', { username, email});
-
-
-
-});*/
 
 export default router;
